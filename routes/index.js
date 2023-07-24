9e47f046acda3b2684058bf3fdabb965339aa81d
karmadosha <carmado@yandex.ru>
2023-07-24 14:25:26 +0800

    route неверный путь

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,8 @@ const cardsRouter = require('./cards');
 
 routes.use('/users', usersRouter);
 routes.use('/cards', cardsRouter);
+routes.use('/', (req, res, next) => {
+  next(res.status(404).send({ message: 'Страница не найдена'}));
+})
 
 module.exports = routes;
